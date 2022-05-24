7c5f9c50b903cec4db730eab223a6e2950b36f91
Chirag Rawat <Chiragr39@gmail.com>
2022-05-24 19:22:16 +0530

    Create Array.py

---
+++ Array.py
@@ -0,0 +1,13 @@
+## PEAK ELEMENT
+
+def peakElement(self,arr, n):
+    # Code here
+    ans = 0
+    if n == 1 or arr[0] > arr[1]:
+        return 0
+    elif arr[n-1] > arr[n-2]:
+        return n-1
+    else:
+        for i in range(1,n-1):
+            if arr[i] > arr[i-1] and arr[i] > arr[i+1]:
+                return i
