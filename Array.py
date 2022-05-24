80b40f335a1dbd5e35c87874f82a281d05c9a386
Chirag Rawat <Chiragr39@gmail.com>
2022-05-24 19:35:10 +0530

    Update Array.py

--- Array.py
+++ Array.py
@@ -12,4 +12,17 @@ def peakElement(self,arr, n):
             if arr[i] > arr[i-1] and arr[i] > arr[i+1]:
                 return i
 
-## 
+## 2. Find minimum and maximum element in an array
+
+def getMinMax( a, n):
+    min_el = a[1]
+    max_el = a[1]
+    
+    for i in a:
+        if i < min_el :
+            min_el = i
+        if i > max_el :
+            max_el = i
+        
+    return [min_el, max_el]
+    
